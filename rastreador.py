ab25efe5de055824c53822fe478041aca51cf2d1
Jose Ramón Caravaca <joseramoncaravaca@gmail.com>
2024-12-21 07:37:29 +0100

    Inicio del proyecto

---
+++ rastreador.py
@@ -0,0 +1,8 @@
+def mostrar_menu():
+    print("Bienvenido al Rastreador de Habitos")
+    print("1. Añadir un nuevo hábito")
+    print("2. Marcar hábito como completado")
+    print("3. Ver progreso diario")
+    print("4. Salir")
+
+mostrar_menu()
