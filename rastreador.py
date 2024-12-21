d9246dadebacc9f87a87c0d3103f538440873177
Jose Ramón Caravaca <joseramoncaravaca@gmail.com>
2024-12-21 07:45:06 +0100

    Función de bucle de interacción con el usuario

--- rastreador.py
+++ rastreador.py
@@ -5,4 +5,22 @@ def mostrar_menu():
     print("3. Ver progreso diario")
     print("4. Salir")
 
-mostrar_menu()
+def main():
+    while True:
+        mostrar_menu()
+        opcion = int(input("Introduce una opción: "))
+    
+        if opcion == 1:
+            print("Has elegido un nuevo hábito (pendiente)")
+        elif opcion == 2: 
+            print("Has elegido marcar un hábito como completado(pendiente)")
+        elif opcion == 3:
+            print("Has elegido ver tu progreso con un hábito (pendiente)")
+        elif opcion == 4: 
+            print("Saliendo del programa")
+            break
+        else:
+            print("Opcion invalida, intentalo de nuevo")
+            
+
+main()
