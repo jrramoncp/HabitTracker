1eb5398c66fa313b0349c41f65d4986f00798684
Jose Ramón Caravaca <joseramoncaravaca@gmail.com>
2024-12-22 06:00:25 +0100

    Finalizo la funcion actualizar habito

--- rastreador.py
+++ rastreador.py
@@ -10,15 +10,15 @@ def main():
     #Bucle Principal interfaz CLI
     while True:
         mostrar_menu()
-        opcion = int(input("Introduce una opción: "))
+        opcion = input("Introduce una opción: ")
     
-        if opcion == 1:
-            print("Has elegido un nuevo hábito (pendiente)")
-        elif opcion == 2: 
-            print("Has elegido marcar un hábito como completado(pendiente)")
-        elif opcion == 3:
+        if int(opcion) == 1:
+            anadir_habito()
+        elif int(opcion) == 2: 
+            actualizar_habito()
+        elif int(opcion) == 3:
             print("Has elegido ver tu progreso con un hábito (pendiente)")
-        elif opcion == 4: 
+        elif int(opcion) == 4: 
             print("Saliendo del programa")
             break
         else:
@@ -26,7 +26,7 @@ def main():
             
 habitos = {} #diccionario de habitos, (habito : estado)
 
-def añadir_habito():
+def anadir_habito():
     #Funcion para añadir un hábito nuevo
     nombre = input("Introduce tu nuevo hábito: ")
     habitos[nombre] = "Pendiente"
@@ -34,7 +34,7 @@ def añadir_habito():
 
 def actualizar_habito():
     #Funcion para actualizar el estado del habito
-    nombre = input("Introduce el hábito que quieres marcar como completado")
+    nombre = input("Introduce el hábito que quieres marcar como completado: ")
     if nombre in habitos:
         #Si el nombre coincide con alguno del diccionario, cambio su valor a "Completado"
         habitos[nombre] = "Completado"
@@ -42,22 +42,22 @@ def actualizar_habito():
     else:
         #Si el nombre introducio no coincide, imprime mensaje de error
         print("No estas haciendos siguimiento de ese hábito")
-        nuevo = input("¿Quieres añadirlo como nuevo hábito? Si | No")
-        #Da la posibilidad de añadir un nuevo habito al diccionario
-        if nuevo.lower() == "si":
-            #Si el usuario indica que si, añade el nuevo habito al diccionario
-            habitos[nombre] = "Pendiente"
-        if nuevo.lower() == "no":
-            print("De acuerdo... volviendo al menú principal")
-        ###Bucle para que vuelva al inicio??
-        ###Bucle si se introduce algo que no debería? 
-
-añadir_habito()
-print(habitos)
-print("-----")
-
-actualizar_habito()
-print(habitos)
+        while True:
+            nuevo = input("¿Quieres añadirlo como nuevo hábito? Si | No ")
+            #Da la posibilidad de añadir un nuevo habito al diccionario
+            if nuevo.lower() == "si":
+                #Si el usuario indica que si, añade el nuevo habito al diccionario
+                habitos[nombre] = "Pendiente"
+                break
+            if nuevo.lower() == "no":
+                print("De acuerdo... volviendo al menú principal")
+                break
+            else: 
+                print("Respuesta no válida, vuelve a intentarlo")
 
 
-#main()
+
+
+
+
+main()
